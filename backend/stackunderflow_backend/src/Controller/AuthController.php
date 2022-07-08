abb47c8806ab0fcdbafebabb2574f2e9e3a2f969
michaelernst18 <107046781+michaelernst18@users.noreply.github.com>
2022-07-08 13:49:44 +0200

    Added login for the backend

--- backend/stackunderflow_backend/src/Controller/AuthController.php
+++ backend/stackunderflow_backend/src/Controller/AuthController.php
@@ -2,26 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AuthController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(Request $request, UserPasswordHasherInterface $passwordHasher, UserRepository $userRepository)
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        $requestUser = json_decode($request->getContent(), false);
+        $user = $userRepository->findOneBy(["email" => $requestUser->email]);
 
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+        if (!$user) {
+            return new Response("User with email: " . $requestUser->email . " not found");
+        }
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        if (!$passwordHasher->isPasswordValid($user, $requestUser->password)) {
+            return new Response("Password is not valid try again");
+        }
+
+        return new Response("Welcome");
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
